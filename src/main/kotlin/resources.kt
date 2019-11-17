e476c391f229c8fc7dd266c3a70902851bd7ff66
kilikfan <mark_fisher1991@yahoo.co.uk>
2019-11-17 12:57:24 +0000

    Initial commit

--- src/main/kotlin/resources.kt
+++ src/main/kotlin/resources.kt
@@ -1,3 +1,4 @@
+//TODO - turn this into a data class
 class Hand(var map: Int = 0, var crew: Int = 2, var landlubber: Int = 2, var food: Int = 2, var doubloon: Int = 2,
            var infamy: Int = 0
 ) {
