d63901e70021b876fbccacbc882a1d77d8dfc51b
kilikfan <mark_fisher1991@yahoo.co.uk>
2020-03-01 17:32:20 +0000

    Cleanup

--- src/main/kotlin/main.kt
+++
@@ -1,8 +0,0 @@
-fun main(args: Array<String>){
-    println("Hello Player!")
-    println("Welcome to Treasure of the High Seas!")
-    println("")
-    println("Let's get ye started with a few resources!")
-    var playerHand = Hand()
-    println("$playerHand")
-}
--- src/main/kotlin/resources.kt
+++
@@ -1,6 +0,0 @@
-//TODO - turn this into a data class
-class Hand(var map: Int = 0, var crew: Int = 2, var landlubber: Int = 2, var food: Int = 2, var doubloon: Int = 2,
-           var infamy: Int = 0
-) {
-    override fun toString(): String = "Maps: $map\tCrew: $crew\tLandlubbers: $landlubber\tFood: $food\tDoubloons: $doubloon\tInfamy: $infamy"
-}
