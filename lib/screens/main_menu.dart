e9211f94746bb03f510133c3e9ac9d3b7acb47e2
Jonathan Masson <jmasson91@googlemail.com>
2020-04-04 17:52:57 +0100

    Layout of main menu buttons

--- lib/screens/main_menu.dart
+++ lib/screens/main_menu.dart
@@ -11,21 +11,51 @@ class MainMenuPage extends StatelessWidget {
         title: Text('Treasure of the High Seas'),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            RaisedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => PlayPage(title: 'Play Page')),
-                );
-              },
-              child: const Text(
-                'Play'
+        child: ButtonTheme(
+          height: 100,
+          minWidth: (MediaQuery.of(context).size.width) * 0.8,       
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: RaisedButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => PlayPage(title: 'Play Page')),
+                    );
+                  },
+                  color: Colors.green,
+                  child: const Text(
+                    'Play',
+                    style: TextStyle(fontSize: 30)
+                  ),
+                ),
               ),
-            )
-          ],
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: RaisedButton(
+                  onPressed: null,
+                  child: const Text(
+                    'Achievements',
+                    style: TextStyle(fontSize: 30)
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: RaisedButton(
+                  onPressed: null,
+                  child: const Text(
+                    'Settings',
+                    style: TextStyle(fontSize: 30)
+                  ),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
