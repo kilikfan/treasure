2d5ac3b8b11e23e2893ce00a27612e5ebcfb9687
Alex Burlton <alexburlton@hotmail.com>
2020-04-12 09:56:55 +0100

    Tests for replace action

---
+++ test/model/card/action/replace_action_test.dart
@@ -0,0 +1,42 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:treasure_of_the_high_seas/model/card/action/replace_action.dart';
+import 'package:treasure_of_the_high_seas/model/card/basic/plunder_a_wreck.dart';
+import 'package:treasure_of_the_high_seas/model/card/quest/hispaniola_1_rumours_of_an_island.dart';
+import 'package:treasure_of_the_high_seas/model/card/quest/hispaniola_2_land_ahoy.dart';
+import 'package:treasure_of_the_high_seas/model/resource.dart';
+
+import '../../../test_utils.dart';
+
+main() {
+  test('should exile the current card and add replacement to the discard pile', () {
+    const rumoursOfAnIsland = RumoursOfAnIsland();
+    const landAhoy = LandAhoy();
+    const plunderAWreck = PlunderAWreck();
+
+    final state = makeGameState(playerHand: [Resource.CREW], deck: [rumoursOfAnIsland, plunderAWreck]);
+    final currentCard = state.nextCard();
+
+    final action = new ReplaceAction(landAhoy);
+    action.performAction(state);
+
+    expect(state.exile, [currentCard]);
+    expect(state.discard, [landAhoy]);
+    expect(state.playerHand.cards, [Resource.CREW]);
+  });
+
+  test('should support an optional cost', () {
+    const rumoursOfAnIsland = RumoursOfAnIsland();
+    const landAhoy = LandAhoy();
+    const plunderAWreck = PlunderAWreck();
+
+    final state = makeGameState(playerHand: [Resource.CREW], deck: [rumoursOfAnIsland, plunderAWreck]);
+    final currentCard = state.nextCard();
+
+    final action = new ReplaceAction(landAhoy, [Resource.CREW]);
+    action.performAction(state);
+
+    expect(state.exile, [currentCard]);
+    expect(state.discard, [landAhoy]);
+    expect(state.playerHand.cards, []);
+  });
+}
