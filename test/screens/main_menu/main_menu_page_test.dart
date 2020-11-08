688f40aa28f11df009f510bb8e02b4aa33730765
Alex Burlton <alexburlton@hotmail.com>
2020-11-08 14:49:38 +0000

    new game test

--- test/screens/main_menu/main_menu_page_test.dart
+++ test/screens/main_menu/main_menu_page_test.dart
@@ -1,6 +1,8 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:treasure_of_the_high_seas/model/game_state_factory.dart';
 import 'package:treasure_of_the_high_seas/screens/main_menu/main_menu_page.dart';
 import 'package:treasure_of_the_high_seas/screens/main_menu/menu_button.dart';
+import 'package:treasure_of_the_high_seas/screens/play/play_page.dart';
 import 'package:treasure_of_the_high_seas/screens/play/rules.dart';
 
 import '../../test_utils.dart';
@@ -15,6 +17,23 @@ void main() {
     await tester.tap(rulesButtonFinder);
     await tester.pumpAndSettle();
 
-    expect(find.byType(RulesPage, skipOffstage: true), findsOneWidget);
+    expect(find.byType(RulesPage), findsOneWidget);
+  });
+
+  testWidgets('should start a new game', (WidgetTester tester) async {
+    await tester.pumpWidget(createWidgetForTesting(child: MainMenuPage()));
+
+    final newGameFinder = find.widgetWithText(MenuButton, 'Play');
+    expect(newGameFinder, findsOneWidget);
+
+    await tester.tap(newGameFinder);
+    await tester.pumpAndSettle();
+
+    expect(find.byType(PlayPage), findsOneWidget);
+
+    final playPage = tester.widget<PlayPage>(find.byType(PlayPage));
+    final gameState = playPage.state;
+    expect(gameState.playerHand.cards, INITIAL_RESOURCES);
+    expect(gameState.currentCard, isNotNull);
   });
 }
