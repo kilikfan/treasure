f2dab8911be5d304d9450e97005a237614f75578
Alex Burlton <alexburlton@hotmail.com>
2021-01-13 20:25:49 +0000

    Use ChangeNotifierProvider in tests

---
+++ test/screens/play/card/card_display_test.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:treasure_of_the_high_seas/model/card/basic/plunder_a_wreck.dart';
+import 'package:treasure_of_the_high_seas/model/card/card_types.dart';
+import 'package:treasure_of_the_high_seas/model/card/quest/hispaniola_1_rumours_of_an_island.dart';
+import 'package:treasure_of_the_high_seas/model/card/special/mutiny.dart';
+import 'package:treasure_of_the_high_seas/screens/play/card/card_display.dart';
+
+import '../../../test_utils.dart';
+
+void main() {
+  testWidgets('should change colour based on the card type displayed', (WidgetTester tester) async {
+    final cardFinder = find.byType(Card);
+
+    await tester.pumpWidget(createWidgetForTesting(child: CardDisplay(PlunderAWreck()), state: makeGameState()));
+    final basicCard = tester.widget<Card>(cardFinder);
+    expect(basicCard.color, basicCardColour);
+
+    await tester.pumpWidget(createWidgetForTesting(child: CardDisplay(RumoursOfAnIsland()), state: makeGameState()));
+    final questCard = tester.widget<Card>(cardFinder);
+    expect(questCard.color, questCardColour);
+
+    await tester.pumpWidget(createWidgetForTesting(child: CardDisplay(Mutiny()), state: makeGameState()));
+    final specialCard = tester.widget<Card>(cardFinder);
+    expect(specialCard.color, specialCardColour);
+  });
+}
