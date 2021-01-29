8c7a400916d753688132def453f23420e6bf487d
kilikfan <mark_fisher1991@yahoo.co.uk>
2021-01-29 19:22:18 +0000

    Rejigged where the close button is added and made it crash!

--- lib/screens/play/view_discard_page.dart
+++ lib/screens/play/view_discard_page.dart
@@ -5,34 +5,31 @@ import '../../model/card/card.dart' as Model;
 import 'card_viewer.dart';
 
 class ViewDiscardPage extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
-    final discard = context.select<GameState, List<Model.Card>>((state) => state.discard);
+    final discard =
+        context.select<GameState, List<Model.Card>>((state) => state.discard);
 
-    return CardViewer(discard, closeButton);
+    return Column(children: <Widget>[
+      CardViewer(discard, buttons),
+      Container(
+          padding: EdgeInsets.symmetric(vertical: 5),
+          width: MediaQuery.of(context).size.width * 0.7,
+          child: RaisedButton(
+            shape: RoundedRectangleBorder(
+              side: BorderSide(color: Colors.black, width: 1),
+              borderRadius: BorderRadius.circular(10),
+            ),
+            onPressed: () {
+              context.read<GameState>().toggleDiscardView();
+            },
+            color: Colors.green,
+            child: Text("Close", style: TextStyle(fontSize: 30)),
+          ))
+    ]);
   }
 
-  List<Widget> closeButton(context, card){
-    return [_getCloseButton(context, card)];
-  }
-
-  Widget _getCloseButton(
-      BuildContext context, Model.Card card) {
-    return Container(
-        padding: EdgeInsets.symmetric(vertical: 5),
-        width: MediaQuery.of(context).size.width * 0.7,
-        child: RaisedButton(
-          shape: RoundedRectangleBorder(
-            side: BorderSide(color: Colors.black, width: 1),
-            borderRadius: BorderRadius.circular(10),
-          ),
-          onPressed: () {
-            context.read<GameState>().toggleDiscardView();
-          },
-          color: Colors.green,
-          child:
-              Text("Close", style: TextStyle(fontSize: 30)),
-        ));
+  List<Widget> buttons(context, card) {
+    return [];
   }
 }
