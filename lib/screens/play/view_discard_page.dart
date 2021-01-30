b9652ecb16cdbb7d3c0fc0375a54c2b4082cc4e7
kilikfan <mark_fisher1991@yahoo.co.uk>
2021-01-30 15:25:12 +0000

    Fixed the crash and reversed the pile order.

--- lib/screens/play/view_discard_page.dart
+++ lib/screens/play/view_discard_page.dart
@@ -8,10 +8,10 @@ class ViewDiscardPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final discard =
-        context.select<GameState, List<Model.Card>>((state) => state.discard);
+        context.select<GameState, List<Model.Card>>((state) => state.discard.reversed.toList());
 
     return Column(children: <Widget>[
-      CardViewer(discard, buttons),
+      Expanded(child: CardViewer(discard, buttons)),
       Container(
           padding: EdgeInsets.symmetric(vertical: 5),
           width: MediaQuery.of(context).size.width * 0.7,
