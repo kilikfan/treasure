317406e03f2fc82217588dd61f3ce06efd5d15e9
kilikfan <mark_fisher1991@yahoo.co.uk>
2021-02-07 18:33:10 +0000

    Plugged in settings, added text wrapper class to set font.

--- lib/screens/quick_rules_page.dart
+++ lib/screens/quick_rules_page.dart
@@ -11,31 +11,29 @@ class QuickRulesPage extends StatelessWidget {
             padding: const EdgeInsets.all(16.0),
             child:
                 Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-              Text(
+              _plainText(
                   "Object\nComplete a quest line by managing your resources to win!\n"),
-              Text(
+              _plainText(
                   "Resources\nM - Maps - Valuable resource and can also act as a wildcard"),
-              Text("C - Crew - Basic resource, experienced seafarers"),
-              Text("L - Landlubbers - Basic resource, inexperienced seafarers"),
-              Text("F - Food - Basic resource, needed to sustain your crew"),
-              Text("D - Doubloons - Basic resource, the local currency"),
-              Text("I - Infamy - Special resource tracking unwanted attention"),
-              Text("\nSymbols"),
+              _plainText("C - Crew - Basic resource, experienced seafarers"),
+              _plainText("L - Landlubbers - Basic resource, inexperienced seafarers"),
+              _plainText("F - Food - Basic resource, needed to sustain your crew"),
+              _plainText("D - Doubloons - Basic resource, the local currency"),
+              _plainText("I - Infamy - Special resource tracking unwanted attention"),
+              _plainText("\nSymbols"),
               _iconText(Icons.arrow_downward, Colors.red, "Cost of an action"),
               _iconText(
                   Icons.arrow_upward, Colors.green, "Reward from an action"),
-                  _iconText(
-                      Icons.description, Colors.purpleAccent, "The named card will go into the discard pile, either gaining or losing quest progress"),
-                  _iconText(
-                      Icons.remove_red_eye, Colors.blue, "Scry (See and reorder the top cards of the deck)"),
-                  _iconText(
-                      Icons.replay, Colors.black, "This card will go into the discard pile"),
-                  _iconText(
-                      Icons.delete, Colors.black, "This card will be removed, exiled, from the deck"),
-                  _iconText(
-                      Icons.check, Colors.green, "You win the game!"),
-                  _iconText(
-                      Icons.close, Colors.red, "You lose the game!"),
+              _iconText(Icons.description, Colors.purpleAccent,
+                  "The named card will go into the discard pile, either gaining or losing quest progress"),
+              _iconText(Icons.remove_red_eye, Colors.blue,
+                  "Scry (See and reorder the top cards of the deck)"),
+              _iconText(Icons.replay, Colors.black,
+                  "This card will go into the discard pile"),
+              _iconText(Icons.delete, Colors.black,
+                  "This card will be removed, exiled, from the deck"),
+              _iconText(Icons.check, Colors.green, "You win the game!"),
+              _iconText(Icons.close, Colors.red, "You lose the game!"),
             ])));
   }
 
@@ -52,6 +50,13 @@ class QuickRulesPage extends StatelessWidget {
           ),
         ],
       ),
+    );
+  }
+
+  Widget _plainText(String text) {
+    return Text(
+      text,
+      style: TextStyle(color: Colors.black, fontSize: 16),
     );
   }
 }
