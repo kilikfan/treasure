28f400b046e3ac3ba9817a0915bfdc9ed4a473a0
Alex Burlton <alexburlton@hotmail.com>
2021-03-03 08:05:01 +0000

    add credits text

--- lib/screens/credits_page.dart
+++ lib/screens/credits_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:treasure_of_the_high_seas/screens/text_utils.dart';
 
 class CreditsPage extends StatelessWidget {
   const CreditsPage() : super();
@@ -13,7 +14,19 @@ class CreditsPage extends StatelessWidget {
           padding: const EdgeInsets.all(16.0),
           child: new SingleChildScrollView(
               child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start, children: [])),
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                heading('Development'),
+                    SizedBox(height: 10),
+                iconText(Icons.anchor, Colors.black, 'Mark Fisher'),
+                iconText(Icons.anchor, Colors.black, 'Alex Burlton'),
+                iconText(Icons.anchor, Colors.black, 'Jon Masson'),
+                iconText(Icons.anchor, Colors.black, 'Samuel Bradley'),
+                    SizedBox(height: 50),
+                    heading('Music / SFX'),
+                    SizedBox(height: 10),
+                    iconText(Icons.music_note, Colors.black, 'Shane Ivers - "The Buccaneer\'s Haul" \n(https://www.silvermansound.com)')
+              ])),
         ));
   }
 }
