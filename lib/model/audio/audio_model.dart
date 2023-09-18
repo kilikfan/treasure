4d05c51b55eb8abce094ae6f01575ebde510b747
kilikfan <mark_fisher1991@yahoo.co.uk>
2023-09-18 10:31:40 +0100

    Attempting to update a lot of things, but it still doesn't build

--- lib/model/audio/audio_model.dart
+++ lib/model/audio/audio_model.dart
@@ -1,4 +1,3 @@
-import 'package:audioplayers/audio_cache.dart';
 import 'package:audioplayers/audioplayers.dart';
 import 'package:treasure_of_the_high_seas/model/settings/settings_model.dart';
 
@@ -7,19 +6,18 @@ import 'audio_constants.dart';
 class AudioModel {
   final SettingsModel _settingsModel;
   final AudioPlayer _musicPlayer;
-  final AudioCache _musicCache;
-  final AudioCache _soundCache;
+  final AudioPlayer _soundPlayer;
   var _playingMusic = false;
   var _lastRequestedMusic = MENU_MUSIC;
 
-  AudioModel(this._settingsModel, this._musicPlayer, this._musicCache, this._soundCache) {
+  AudioModel(this._settingsModel, this._musicPlayer, this._soundPlayer) {
     _settingsModel.addListener(_settingsChanged);
   }
 
   Future<void> loopMusic(String track) async {
     _lastRequestedMusic = track;
     if (_settingsModel.isSettingEnabled(AppSetting.musicEnabled)) {
-      await _musicCache.loop(track, volume: 0.5);
+      await _musicPlayer.play(AssetSource(track), volume: 0.5);
       _playingMusic = true;
     }
   }
@@ -36,7 +34,7 @@ class AudioModel {
 
   Future<void> playSound(String sound) async {
     if (_settingsModel.isSettingEnabled(AppSetting.sfxEnabled)) {
-      await _soundCache.play(sound);
+      await _soundPlayer.play(AssetSource(sound));
     }
   }
 
